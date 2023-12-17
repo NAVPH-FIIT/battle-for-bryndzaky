781cd1a7f8a626386c16af1654c851642ca80af7
PatrikSK <patrik.nemsovsky@gmail.com>
2023-12-17 17:03:58 +0100

    prepared healtbar stuff

--- Assets/Bosses/BossHealth.cs
+++ Assets/Bosses/BossHealth.cs
@@ -1,12 +1,25 @@
+using Bryndzaky.Combat.Spells;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealth : MonoBehaviour
 {
     [SerializeField]
-    private float health = 100;
-//Start is called before the first frame update
+    private float maxHealth = 100;
+    private float health;
+    [SerializeField]
+    private GameObject healthbar;
+    [SerializeField]
+    private Slider healthSlider;
+    //Start is called before the first frame update
+
+    private void Start()
+    {
+        healthbar.SetActive(true);
+        health = maxHealth;
+    }
     void Update()
     {
         if (health <= 0)
@@ -18,6 +31,13 @@ public class BossHealth : MonoBehaviour
     public void Damage(int dealt)
     {
         health -= dealt;
+        if (this.healthSlider != null)
+            this.healthSlider.value = (float)health / maxHealth;
         Debug.Log("Auu");
+    }
+
+    private void OnDestroy()
+    {
+        healthbar.SetActive(false);
     }
 }
